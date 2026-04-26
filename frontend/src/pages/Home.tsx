f31ea2403dde295d427d3c6282bae5772a0640b5
Jürgen <j.mueller@thesolution.at>
2026-04-26 16:28:14 +0200

    fix: total rewrite of Home.tsx to clear syntax errors

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -54,8 +54,6 @@ export function Home() {
         ))}
       </div>
 
-
-
       {/* Decorative background element */}
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[800px] h-[800px] bg-teal/5 rounded-full blur-[120px] pointer-events-none" />
     </div>
